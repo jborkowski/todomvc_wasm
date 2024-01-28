b87a74c88d24906d1709508d685d5ee6871afd2c
Jonatan Borkowski <jonatan@thebo.me>
2024-01-28 23:10:06 +0100

    add toggle logic

--- src/main.rs
+++ src/main.rs
@@ -14,39 +14,61 @@ fn app() -> Html {
                 let input = event.target_dyn_into::<HtmlInputElement>();
 
                 if let Some(input) = input {
-                    let todos = todos.clone();
+                    let mut todos_arr = (*todos).clone();
+                    todos_arr.push(Todo::new(input.value()));
 
-                    let mut prev = (*todos).clone();
-                    prev.push(Todo::new(input.value()));
-
-                    todos.set(prev);
-                    info!("{}", input.value());
+                    todos.set(todos_arr);
                 }
             }
+        })
+    };
+
+    let ontoggle = {
+        let todos = todos.clone();
+
+        Callback::from(move |todo| {
+            let mut todos_arr: Vec<Todo> = (*todos).clone();
+
+            todos_arr.iter_mut().for_each(|t| {
+                if *t == todo {
+                    t.toggle();
+                }
+            });
+
+            info!("Toggled {:?}", todos_arr.clone());
+            todos.set(todos_arr);
         })
     };
 
     html! {
         <div>
             <TodoHeader onkeydown={on_new_todo}  />
-            <TodoMain todos={(*todos).clone()}/>
+            <TodoMain todos={(*todos).clone()} ontoggle={ontoggle}/>
         </div>
     }
 }
 
 #[function_component(TodoMain)]
 fn todo_main(props: &TodosProps) -> Html {
-    let active_todo = props
-        .todos
-        .iter()
-        .filter(|todo| !todo.completed)
-        .enumerate()
-        .map(|(index, todo)| {
-            html! {
-                <TodoItem todo={(*todo).clone()} index={index}/>
-            }
-        })
-        .collect::<Html>();
+    let ontoggle = {
+        let ontoggle = props.ontoggle.clone();
+        move |todo| {
+            let ontoggle = ontoggle.clone();
+            ontoggle.emit(todo)
+        }
+    };
+
+    let active_todo =
+        props
+            .todos
+            .iter()
+            .filter(|todo| !todo.completed)
+            .map(|todo| {
+                html! {
+                    <TodoItem todo={(*todo).clone()}  ontoggle={ontoggle.clone()} />
+                }
+            })
+            .collect::<Html>();
 
     html! {
         <div>
@@ -60,27 +82,39 @@ fn todo_main(props: &TodosProps) -> Html {
     }
 }
 
-#[derive(Properties, PartialEq)]
+#[derive(Properties, PartialEq, Clone)]
 struct TodoItemProps {
     // on_save: Callback<Todo>,
     // on_remove: Callback<usize>,
-    // on_toggle: Callback<usize>,
+    ontoggle: Callback<Todo>,
     todo: Todo,
-    index: usize,
 }
 
 #[function_component(TodoItem)]
 fn todo_item(props: &TodoItemProps) -> Html {
+    let handle_toggle = {
+        let props = props.clone();
+        move |_| {
+            let props = props.clone();
+            props.ontoggle.emit(props.todo.clone())
+        }
+    };
+
+    let handle_double_click = |event: MouseEvent| info!("On Edit click {:?}", event);
+    let handle_remove = |event| info!("On Remove click {:?}", event);
+
     html! {
         <li>
-        <div>
-        {props.todo.title.clone()}
-        </div>
+            <div class="view">
+                <input class="toggle" type="checkbox" checked={props.todo.completed} onchange={handle_toggle} />
+                <label ondblclick={handle_double_click}>{props.todo.title.clone()}</label>
+                <button class="destroy" onclick={handle_remove} />
+            </div>
         </li>
     }
 }
 
-#[derive(Properties, PartialEq)]
+#[derive(Properties, PartialEq, Clone)]
 pub struct TodoHeaderProps {
     pub onkeydown: Callback<KeyboardEvent>,
 }
@@ -101,12 +135,13 @@ fn main() {
     yew::Renderer::<App>::new().render();
 }
 
-#[derive(Properties, PartialEq)]
+#[derive(Properties, PartialEq, Clone)]
 struct TodosProps {
     todos: Vec<Todo>,
+    ontoggle: Callback<Todo>,
 }
 
-#[derive(Properties, PartialEq, Clone)]
+#[derive(Properties, PartialEq, Clone, Debug)]
 struct Todo {
     title: String,
     completed: bool,
@@ -118,5 +153,9 @@ impl Todo {
             title,
             completed: false,
         }
+    }
+
+    pub fn toggle(&mut self) {
+        self.completed = !self.completed;
     }
 }
